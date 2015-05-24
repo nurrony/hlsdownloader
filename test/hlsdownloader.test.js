45cf8061d290cd76a9c517cb8c86358c13251d08
nmrony <pro.nmrony@gmail.com>
2015-05-25 00:38:48 +0600

    constructor testing finished

--- test/hlsdownloader.test.js
+++ test/hlsdownloader.test.js
@@ -1,11 +1,18 @@
 'use strict';
 
 var expect = require('chai').expect;
+var sinon = require('sinon');
+var request = require('request-promise');
 var HLSDownloader = require('../');
 
 describe('HLSDownloader', function() {
 
   describe('#constructor', function() {
+
+    var downloader = new HLSDownloader({
+      playlistURL: 'http://nmrony.local/hls/example.m3u8'
+    });
+
     it('should throw an error', function() {
       expect(function() {
         new HLSDownloader();
@@ -13,17 +20,12 @@ describe('HLSDownloader', function() {
     });
 
     it('should return an object', function() {
-      var downloader = new HLSDownloader({
-        playlistURL: 'http://nmrony.local/hls/example.m3u8'
-      });
       expect(downloader).to.be.an('object').and.to.be.not.null;
 
     });
 
     it('should have all keys', function() {
-      var downloader = new HLSDownloader({
-        playlistURL: 'http://nmrony.local/hls/example.m3u8'
-      });
+
       expect(downloader).to.have.all.keys(['playlistURL',
         'hostName',
         'errors',
@@ -33,14 +35,11 @@ describe('HLSDownloader', function() {
     });
 
     it('destination key should not be null', function() {
-      var downloader = new HLSDownloader({
+      downloader = new HLSDownloader({
         playlistURL: 'http://nmrony.local/hls/example.m3u8',
         destination: '/test'
       });
       expect(downloader.destination).to.be.not.null;
     });
-  });
-
-  describe('#startDownload', function() {
   });
 });
