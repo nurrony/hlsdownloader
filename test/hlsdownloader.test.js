b4905ad4c22d54ca91a6645f5a6520647826d5f8
Nur Rony <pro.nmrony@gmail.com>
2016-04-21 00:17:29 +0600

    fixed compile script

--- test/hlsdownloader.test.js
+++ test/hlsdownloader.test.js
@@ -3,7 +3,7 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import request from 'request-promise';
-import HLSDownloader from '../hlsParser.es2015';
+import HLSDownloader from '../hlsdownloader';
 
 describe('HLSDownloader', () => {
 
