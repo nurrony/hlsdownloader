37ee58db72248f6f26bb87154830658a309aa3ae
Nur Rony <pro.nmrony@gmail.com>
2016-04-21 21:48:18 +0600

    example rewritten

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ import HLSDownloader from 'hlsdownloader'; //Using ES2015 module
 //var HLSDownloader = require('hlsdownloader').downloader; //using commonJS module
 
 const params = {
-  playlistURL: 'http://nmrony.local/hls/playlist.m3u8', // change it
+  playlistURL: 'http://exmple.com/hls/playlist.m3u8', // change it
   destination:'/tmp' // change it
 };
 const downloader = new HLSDownloader(params);
