7e7edc239da430859fe92d9f8c657414bebfc756
Nur Rony <pro.nmrony@gmail.com>
2016-07-07 01:26:54 +0600

    chore(formatting): formatting code following standard JS style

--- example.js
+++ example.js
@@ -1,9 +1,9 @@
-import HLSDownloader from 'hlsdownloader'; //Using ES2015 module
-//var HLSDownloader = require('hlsdownloader').downloader; //using commonJS module
+import HLSDownloader from 'hlsdownloader' // Using ES2015 module
+// var HLSDownloader = require('hlsdownloader').downloader //using commonJS module
 
 const params = {
   playlistURL: 'http://exmple.com/hls/playlist.m3u8', // change it
-  destination:'/tmp' // change it
-};
-const downloader = new HLSDownloader(params);
-downloader.startDownload((err, msg) => err ? console.log(err) : console.log(msg));
+  destination: '/tmp' // change it
+}
+const downloader = new HLSDownloader(params)
+downloader.startDownload((err, msg) => err ? console.log(err) : console.log(msg))
--- test/hlsdownloader.test.js
+++ test/hlsdownloader.test.js
@@ -1,28 +1,25 @@
-'use strict';
+'use strict'
 
-import { expect } from 'chai';
-import request from 'request-promise';
-import {downloader as HLSDownloader} from '../hlsdownloader';
+import { expect } from 'chai'
+import { downloader as HLSDownloader } from '../hlsdownloader'
 
 describe('HLSDownloader', () => {
-
-  let downloader;
+  let downloader
 
   beforeEach(() => {
     downloader = new HLSDownloader({
       playlistURL: 'http://nmrony.local/hls/example.m3u8'
-    });
-  });
+    })
+  })
 
   describe('#constructor', () => {
     it('should throw an error', () => {
-      expect(() => new HLSDownloader()).to.be.throw(Error);
-    });
+      expect(() => new HLSDownloader()).to.be.throw(Error)
+    })
 
     it('should return an object', () => {
-      expect(downloader).to.be.an('object').and.to.be.not.null;
-
-    });
+      expect(downloader).to.be.an('object').and.to.be.not.null
+    })
 
     it('should have all keys', () => {
       expect(downloader).to.have.all.keys(['playlistURL',
@@ -30,59 +27,59 @@ describe('HLSDownloader', () => {
         'errors',
         'items',
         'destination'
-      ]);
-    });
+      ])
+    })
 
     it('destination key should not be null', () => {
-      downloader.destination = '/test';
-      expect(downloader.destination).to.be.not.null;
-    });
-  });
+      downloader.destination = '/test'
+      expect(downloader.destination).to.be.not.null
+    })
+  })
 
   describe('#parseVariantPlaylist', () => {
     const variantPlaylistContent = `#EXTM3U
-#EXT-X-TARGETDURATION:10
-#EXT-X-VERSION:3
-#EXT-X-MEDIA-SEQUENCE:0
-#EXT-X-PLAYLIST-TYPE:VOD
-#EXTINF:9.00900,
-pure-relative.ts
-#EXTINF:9.00900,
-/other/root-relative.ts
-#EXTINF:9.00900,
-../third/relative.ts
-#EXTINF:9.00900,
-http://www.example.com/other-host.ts
-#EXTINF:9.00900,
-//www.example.com/things/protocol-relative.ts
-#EXT-X-ENDLIST
-    `;
+      #EXT-X-TARGETDURATION:10
+      #EXT-X-VERSION:3
+      #EXT-X-MEDIA-SEQUENCE:0
+      #EXT-X-PLAYLIST-TYPE:VOD
+      #EXTINF:9.00900,
+      pure-relative.ts
+      #EXTINF:9.00900,
+      /other/root-relative.ts
+      #EXTINF:9.00900,
+      ../third/relative.ts
+      #EXTINF:9.00900,
+      http://www.example.com/other-host.ts
+      #EXTINF:9.00900,
+      //www.example.com/things/protocol-relative.ts
+      #EXT-X-ENDLIST
+      `
     beforeEach(() => {
-      downloader.parseVariantPlaylist(variantPlaylistContent);
-    });
+      downloader.parseVariantPlaylist(variantPlaylistContent)
+    })
 
     it('should parse the correct number of items', () => {
-      expect(downloader.items.length).to.equal(6);
-    });
+      expect(downloader.items.length).to.equal(6)
+    })
 
     it('should handle urls with no pathing', () => {
-      expect(downloader.items[0]).to.equal('http://nmrony.local/hls/pure-relative.ts');
-    });
+      expect(downloader.items[0]).to.equal('http://nmrony.local/hls/pure-relative.ts')
+    })
 
     it('should handle urls root relative pathing', () => {
-      expect(downloader.items[1]).to.equal('http://nmrony.local/other/root-relative.ts');
-    });
+      expect(downloader.items[1]).to.equal('http://nmrony.local/other/root-relative.ts')
+    })
 
     it('should handle urls with subdirectory pathing', () => {
-      expect(downloader.items[2]).to.equal('http://nmrony.local/third/relative.ts');
-    });
+      expect(downloader.items[2]).to.equal('http://nmrony.local/third/relative.ts')
+    })
 
     it('should handle urls with absolute urls', () => {
-      expect(downloader.items[3]).to.equal('http://www.example.com/other-host.ts');
-    });
+      expect(downloader.items[3]).to.equal('http://www.example.com/other-host.ts')
+    })
 
     it('should handle protocol relative urls', () => {
-      expect(downloader.items[4]).to.equal('http://www.example.com/things/protocol-relative.ts');
-    });
-  });
-});
+      expect(downloader.items[4]).to.equal('http://www.example.com/things/protocol-relative.ts')
+    })
+  })
+})
