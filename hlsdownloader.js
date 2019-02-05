c92d2c0527abfda681aaf5363b92ad7f5533e514
Chocobozzz <me@florianbigard.com>
2019-02-05 10:12:54 +0100

    Relax URL validation

--- hlsdownloader.js
+++ hlsdownloader.js
@@ -21,41 +21,7 @@ function isValidPlaylist(playlistContent) {
  * @returns {boolean}
  */
 function validateURL(url) {
-  var urlRegex = new RegExp(
-    '^' +
-      // protocol identifier
-      '(?:(?:https?)://)' +
-      // user:pass authentication
-      '(?:\\S+(?::\\S*)?@)?' +
-      '(?:' +
-      // IP address exclusion
-      // private & local networks
-      '(?!(?:10|127)(?:\\.\\d{1,3}){3})' +
-      '(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})' +
-      '(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})' +
-      // IP address dotted notation octets
-      // excludes loopback network 0.0.0.0
-      // excludes reserved space >= 224.0.0.0
-      // excludes network & broacast addresses
-      // (first & last IP address of each class)
-      '(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])' +
-      '(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}' +
-      '(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))' +
-      '|' +
-      // host name
-      '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)' +
-      // domain name
-      '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*' +
-      // TLD identifier
-      '(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))' +
-      ')' +
-      // port number
-      '(?::\\d{2,5})?' +
-      // resource path
-      '(?:/\\S*)?' +
-      '$',
-    'i'
-  )
+  var urlRegex = new RegExp('^(?:(?:https?)://)', 'i')
 
   return urlRegex.test(url)
 }
@@ -98,7 +64,7 @@ class HLSDownloader {
   } = {}) {
     if (!validateURL(playlistURL)) {
       const error = new Error()
-      error.message = 'playListURL is required ' + 'or check if your URL is valid or not!!'
+      error.message = 'playlistURL is required or check if your URL is valid or not!'
       error.name = 'ERR_VALIDATION'
       throw error
     }
