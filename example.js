fa772e6ac81503c27dd46cfc116486c650a0671d
Nur Rony <pro.nmrony@gmail.com>
2023-12-17 12:46:21 +0600

    remove old files

--- example.js
+++
@@ -1,9 +0,0 @@
-import HLSDownloader from 'hlsdownloader' // Using ES2015 module
-// var HLSDownloader = require('hlsdownloader').downloader // using commonJS module
-
-const params = {
-  playlistURL: 'http://example.com/url/to/your/hls.m3u8', // change it
-  destination: '/tmp' // change it
-}
-const downloader = new HLSDownloader(params)
-downloader.startDownload((err, msg) => err ? console.log(err) : console.log(msg))
