0594901e6aeec42bbc62440526fe76a2dff41251
Nur Rony <pro.nmrony@gmail.com>
2024-01-05 01:34:39 +0600

    chore(cleanup): remove unnecessary files and add License file

--- example.js
+++ example.js
@@ -1,9 +1,9 @@
-import HLSDownloader from './index';
+import HLSDownloader from './build/index';
 
 const downloader = new HLSDownloader({
   playlistURL: 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
   concurrency: 5,
-  destination: '/tmp',
+  destination: '/tmp/test',
 });
 
 const download = async () => downloader.startDownload();
