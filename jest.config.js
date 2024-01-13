148b72d87486f0739d030e2cd2f892cca1dc58bc
Nur Rony <pro.nmrony@gmail.com>
2024-01-13 15:46:55 +0600

    chore(test): update test
    [skip release]

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ export default {
   coveragePathIgnorePatterns: ['/node_modules/', '/src/utils/index.js', '/src/lib/index.js', '/tests/mocks/'],
   coverageReporters: ['clover', 'html', 'text'],
 
-  // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
       branches: 60,
@@ -14,7 +13,6 @@ export default {
       statements: 60,
     },
   },
-  // Make calling deprecated APIs throw helpful error messages
   errorOnDeprecated: true,
   testEnvironment: 'node',
   transform: {},
