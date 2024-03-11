573b0a5f32a0befa6169ccb4f485799769d42b09
Nur Rony <pro.nmrony@gmail.com>
2024-03-12 01:13:17 +0600

    chore(utils): export new isNotFunction util method

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 /* istanbul ignore file */
-import { isValidPlaylist, isValidUrl, omit, parseUrl, stripFirstSlash } from './utils';
+import { isValidPlaylist, isValidUrl, omit, parseUrl, stripFirstSlash, isNotFunction } from './utils';
 
 /**
  * Utility module for HLSDownloader
@@ -9,4 +9,4 @@ import { isValidPlaylist, isValidUrl, omit, parseUrl, stripFirstSlash } from './
  * <header><h1>HLSDownloaderUtils</h1></header>
  * <p>Utility module for HLSDownloader</p>
  */
-export default { isValidPlaylist, isValidUrl, omit, parseUrl, stripFirstSlash };
+export default { isValidPlaylist, isValidUrl, omit, parseUrl, stripFirstSlash, isNotFunction };
