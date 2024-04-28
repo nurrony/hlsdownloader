4b97fe8c37f9e1bdc504c680a5c40a0206bb6605
Nur Rony <pro.nmrony@gmail.com>
2024-04-28 21:44:37 +0600

    fix(package): upgrade packages and address secuirty vulnerabilities
    also embrace eslint new configs
    
    fix #162

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from 'globals';
+export default [
+  {
+    files: ['**/*.js'],
+    ignores: ['index.js', 'node_modules', 'extras/**', 'test/**', 'coverage', '.nyc-output'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          impliedStrict: true,
+        },
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 2023,
+      sourceType: 'module',
+    },
+  },
+];
