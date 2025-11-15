7d274bdd1968afc8443793885d10f1b1963c2dd2
Nur Rony <pro.nmrony@gmail.com>
2025-11-15 16:45:12 +0600

    fix(ci): fix github actions

--- jest.setup.js
+++
@@ -1,6 +0,0 @@
-import { LocalStorage } from 'node-localstorage';
-
-// Only polyfill if not already provided
-if (typeof global.localStorage === 'undefined') {
-  global.localStorage = new LocalStorage('./.jest-localstorage');
-}
